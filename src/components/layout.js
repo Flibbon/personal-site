2b1894d804ca98fc52ef5891136c8d26765818ab
frankie-libbon <flibbon@rsglab.com>
2021-02-25 16:54:28 -0500

    Wow that update needed to happen

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,31 +1,17 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
 
 import Header from "./Header/Header";
 import "./Normalize.css";
 import "./Layout.less";
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <Fragment>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div className="content">
-          <main>{children}</main>
-        </div>
-      </Fragment>
-    )}
-  />
+    <Fragment>
+      <Header siteTitle="flibbon" />
+      <div className="content">
+        <main>{children}</main>
+      </div>
+    </Fragment>
 );
 
 Layout.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 import Layout from "../components/Layout";
-import SEO from "../components/SEO";
 
 const Home = () => {
   return (
     <Layout>
-      <SEO title="home" />
       <div className="main">
-        <h2>hi.</h2>
-        <p>i'm a senior software engineer, the tech lead of <a href="https://mailchimp.com/" target="_blank">Mailchimp</a>'s new email editor, and leader of experimentation & growth best practices at Mailchimp.</p>
-        <p>i also used to be a game developer! i created web games from the ground up for companies such as Nickelodeon, Disney, and Nintendo.</p>
-        <p>if you want to say hi, i'm always open to chat <a href="mailto:frankielibbon@gmail.com">frankielibbon@gmail.com</a></p>
+        <h2>hi. i'm frankie libbon.</h2>
+        <p>welcome to personal site v132.9.12</p>
+        <p>i'm a senior software engineer, the tech lead of <a href="https://mailchimp.com/" target="_blank" rel="noopener noreferrer">Mailchimp</a>'s new email editor</p>
+        <p>before that, i was a leader of growth & experimentation engineering best practices at Mailchimp.</p>
+        <p>and before <strong>that</strong>, i created web games from the ground up for companies you might know, including Nickelodeon, Disney, and Nintendo.</p>
+        <p>if you'd like to talk to me, i'm right over here: <a href="mailto:frankielibbon@gmail.com">frankielibbon@gmail.com</a></p>
       </div>
     </Layout>
   );
