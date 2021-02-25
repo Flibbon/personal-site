9c04a1de93fbfc2c00977b9ced9b91e43ea90c70
frankie-libbon <flibbon@rsglab.com>
2021-02-25 17:06:22 -0500

    Bit better

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,11 @@ const Home = () => {
   return (
     <Layout>
       <div className="main">
-        <h2>hi. i'm frankie libbon.</h2>
-        <p>welcome to personal site v132.9.12</p>
-        <p>i'm a senior software engineer, the tech lead of <a href="https://mailchimp.com/" target="_blank" rel="noopener noreferrer">Mailchimp</a>'s new email editor</p>
+        <h2>welcome to personal site v37.265.43</h2>
+        <p>hi. i'm frankie libbon.</p>
+        <p>i'm a senior software engineer and the tech lead of <a href="https://mailchimp.com/" target="_blank" rel="noopener noreferrer">Mailchimp</a>'s new email editor.</p>
         <p>before that, i was a leader of growth & experimentation engineering best practices at Mailchimp.</p>
-        <p>and before <strong>that</strong>, i created web games from the ground up for companies you might know, including Nickelodeon, Disney, and Nintendo.</p>
+        <p>and before <strong>that</strong>, i created web games for companies you might know, including Nickelodeon, Disney, and Nintendo.</p>
         <p>if you'd like to talk to me, i'm right over here: <a href="mailto:frankielibbon@gmail.com">frankielibbon@gmail.com</a></p>
       </div>
     </Layout>
