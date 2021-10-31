c837a3df30e7fde45caa22033c229c4ec66dbbc5
frankie-libbon <flibbon@rsglab.com>
2021-10-31 13:20:10 -0400

    Update some text, color contrast

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,12 @@ const Home = () => {
   return (
     <Layout>
       <div className="main">
-        <h2>welcome to personal site v37.265.43</h2>
-        <p>hi. i'm frankie libbon.</p>
-        <p>i'm a senior software engineer and the tech lead of <a href="https://mailchimp.com/" target="_blank" rel="noopener noreferrer">Mailchimp</a>'s new email editor.</p>
+        <h2>welcome to personal site v4.2</h2>
+        <p>hi. i’m frankie libbon.</p>
+        <p>i’m a <a href="https://www.linkedin.com/in/frankielibbon/" target="_blank" rel="noopener noreferrer">senior software engineer</a> and the tech lead of Mailchimp’s new email editor.</p>
         <p>before that, i was a leader of growth & experimentation engineering best practices at Mailchimp.</p>
         <p>and before <strong>that</strong>, i created web games for companies you might know, including Nickelodeon, Disney, and Nintendo.</p>
-        <p>if you'd like to talk to me, i'm right over here: <a href="mailto:frankielibbon@gmail.com">frankielibbon@gmail.com</a></p>
+        <p>if you’d like to talk to me, i’m right over here: <a href="mailto:frankielibbon@gmail.com">frankielibbon@gmail.com</a></p>
       </div>
     </Layout>
   );
--- src/pages/libbon-life.js
+++ src/pages/libbon-life.js
@@ -6,8 +6,8 @@ import SEO from "../components/SEO";
 const About = () => (
   <Layout>
     <SEO title="about" />
-    <h2>it's me.</h2>
-    <p>this will be a contact page I guess. we'll get there.</p>
+    <h2>it’s me.</h2>
+    <p>this will be a contact page I guess. we’ll get there.</p>
   </Layout>
 );
 
